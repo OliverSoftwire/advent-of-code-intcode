db0f9d27f0efaf58b61976a90924f61de91065b2
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2023-03-15 10:19:02 +0000

    Test helper and more tests

--- tests/05.spec.ts
+++ tests/05.spec.ts
@@ -18,89 +18,84 @@ describe("Day 5", () => {
 	});
 
 	describe("part 2 examples", () => {
-		it("should pass the position mode equals example", () => {
-			// Given
-			const program = "3,9,8,9,10,9,4,9,99,-1,8";
+		function testProgram(
+			name: string,
+			program: string,
+			inputs: number[],
+			expectedOutputs: number[]
+		) {
+			it(name, () => {
+				// Given
+				const vm = new IntcodeVM();
+				vm.loadProgram(program);
 
-			const vm = new IntcodeVM();
-			vm.loadProgram(program);
+				// When
+				inputs.forEach((input) => {
+					vm.writeInput(input);
+					vm.runUntilComplete();
+					vm.reset();
+				});
 
-			// When
-			vm.writeInput(1);
-			vm.runUntilComplete();
+				// Then
+				expectedOutputs.forEach((expected) =>
+					expect(vm.readOutput()).toBe(expected)
+				);
+				expect(vm.readOutput()).toBeUndefined();
+			});
+		}
 
-			vm.reset();
+		describe("comparisons to 8", () => {
+			testProgram(
+				"should pass the position mode equals example",
+				"3,9,8,9,10,9,4,9,99,-1,8",
+				[1, 8],
+				[0, 1]
+			);
 
-			vm.writeInput(8);
-			vm.runUntilComplete();
+			testProgram(
+				"should pass the position mode less than example",
+				"3,9,7,9,10,9,4,9,99,-1,8",
+				[1, 8],
+				[1, 0]
+			);
 
-			// Then
-			expect(vm.readOutput()).toBe(0);
-			expect(vm.readOutput()).toBe(1);
+			testProgram(
+				"should pass the immediate mode equals example",
+				"3,3,1108,-1,8,3,4,3,99",
+				[1, 8],
+				[0, 1]
+			);
+
+			testProgram(
+				"should pass the immediate mode less than example",
+				"3,3,1107,-1,8,3,4,3,99",
+				[1, 8],
+				[1, 0]
+			);
 		});
 
-		it("should pass the position mode less than example", () => {
-			// Given
-			const program = "3,9,7,9,10,9,4,9,99,-1,8";
+		describe("jump", () => {
+			testProgram(
+				"should pass the position mode jump example",
+				"3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9",
+				[0, 1],
+				[0, 1]
+			);
 
-			const vm = new IntcodeVM();
-			vm.loadProgram(program);
-
-			// When
-			vm.writeInput(1);
-			vm.runUntilComplete();
-
-			vm.reset();
-
-			vm.writeInput(8);
-			vm.runUntilComplete();
-
-			// Then
-			expect(vm.readOutput()).toBe(1);
-			expect(vm.readOutput()).toBe(0);
+			testProgram(
+				"should pass the immediate mode jump example",
+				"3,3,1105,-1,9,1101,0,0,12,4,12,99,1",
+				[0, 1],
+				[0, 1]
+			);
 		});
 
-		it("should pass the immediate mode equals example", () => {
-			// Given
-			const program = "3,3,1108,-1,8,3,4,3,99";
-
-			const vm = new IntcodeVM();
-			vm.loadProgram(program);
-
-			// When
-			vm.writeInput(1);
-			vm.runUntilComplete();
-
-			vm.reset();
-
-			vm.writeInput(8);
-			vm.runUntilComplete();
-
-			// Then
-			expect(vm.readOutput()).toBe(0);
-			expect(vm.readOutput()).toBe(1);
-		});
-
-		it("should pass the immediate mode less than example", () => {
-			// Given
-			const program = "3,3,1107,-1,8,3,4,3,99";
-
-			const vm = new IntcodeVM();
-			vm.loadProgram(program);
-
-			// When
-			vm.writeInput(1);
-			vm.runUntilComplete();
-
-			vm.reset();
-
-			vm.writeInput(8);
-			vm.runUntilComplete();
-
-			// Then
-			expect(vm.readOutput()).toBe(1);
-			expect(vm.readOutput()).toBe(0);
-		});
+		testProgram(
+			"should pass complex example",
+			"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99",
+			[1, 8, 10],
+			[999, 1000, 1001]
+		);
 	});
 
 	it("should solve part 1", () => {
