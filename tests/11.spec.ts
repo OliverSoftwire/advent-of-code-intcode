ea2dce708af178137f5ae8962e222ac5e0576111
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2023-03-29 11:07:44 +0100

    Add day 11 part tests

---
+++ tests/11.spec.ts
@@ -0,0 +1,19 @@
+import solution from "../src/solutions/day11";
+
+describe("Day 9", () => {
+	it("should solve part 1", () => {
+		// When
+		const result = solution.runPart1();
+
+		// Then
+		expect(result).toBe(1876);
+	});
+
+	xit("should solve part 2", () => {
+		// When
+		const result = solution.runPart2();
+
+		// Then
+		expect(result).toBe(0);
+	});
+});
