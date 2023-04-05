fa0c212c0dedbfe2ee6016ce8df558d49a195b2d
olispe-fdg <115625185+olispe-fdg@users.noreply.github.com>
2023-04-05 12:14:20 +0100

    Refactor arcade cabinet into class

--- src/solutions/day13.ts
+++ src/solutions/day13.ts
@@ -30,40 +30,103 @@ const palette = {
 	},
 };
 
-interface PaintCommand {
-	coordinate: Vector2;
-	paletteIndex: PaletteIndex;
+interface Command {
+	x: number;
+	y: number;
+	value: number;
 }
 
-function getNextPaintCommand(vm: IntcodeVM): PaintCommand | undefined {
-	const x = vm.runUntilOutput();
-	const y = vm.runUntilOutput();
-	const paletteIndex = vm.runUntilOutput();
+interface PaintCommand extends Command {
+	value: PaletteIndex;
+}
 
-	if (_.isUndefined(x) || _.isUndefined(y) || _.isUndefined(paletteIndex)) {
-		return undefined;
+interface ScoreCommand extends Command {
+	x: -1;
+	y: 0;
+}
+
+enum JoystickCommands {
+	Left = -1,
+	Neutral = 0,
+	Right = 1,
+}
+
+function commandIsPaintCommand(command: Command): command is PaintCommand {
+	return Object.values(PaletteIndex).includes(command.value);
+}
+
+function commandIsScoreCommand(command: Command): command is ScoreCommand {
+	return command.x === -1 && command.y === 0;
+}
+
+class Cabinet {
+	private vm = new IntcodeVM();
+
+	private paddlePosition = new Vector2(0, 0);
+	public score = 0;
+
+	constructor(program: string, runWithQuarters?: boolean) {
+		this.vm.loadProgram(program);
+
+		if (runWithQuarters) {
+			this.vm.writeMemory(0, 2);
+		}
 	}
 
-	return {
-		coordinate: new Vector2(x, y),
-		paletteIndex,
-	};
+	public runWith(onPaint: (command: PaintCommand) => void) {
+		let command;
+		while ((command = this.getNextCommand())) {
+			if (commandIsScoreCommand(command)) {
+				this.score = command.value;
+				continue;
+			}
+
+			if (commandIsPaintCommand(command)) {
+				if (command.value === PaletteIndex.HorizontalPaddle) {
+					this.paddlePosition = new Vector2(command.x, command.y);
+				}
+
+				onPaint(command);
+			}
+		}
+	}
+
+	private getNextCommand(): Command | undefined {
+		const x = this.vm.runUntilOutput();
+		const y = this.vm.runUntilOutput();
+		const value = this.vm.runUntilOutput();
+
+		if (_.isUndefined(x) || _.isUndefined(y) || _.isUndefined(value)) {
+			return undefined;
+		}
+
+		return {
+			x,
+			y,
+			value,
+		};
+	}
 }
 
 function solution1(program: string): number {
-	const vm = new IntcodeVM();
-	vm.loadProgram(program);
+	const cabinet = new Cabinet(program);
 
 	let numBlocks = 0;
-
-	let command;
-	while ((command = getNextPaintCommand(vm))) {
-		if (command.paletteIndex === PaletteIndex.Block) {
+	cabinet.runWith(({ value }) => {
+		if (value === PaletteIndex.Block) {
 			numBlocks++;
 		}
-	}
+	});
 
 	return numBlocks;
 }
 
-export default new Solution(13, solution1);
+function solution2(program: string): number {
+	const cabinet = new Cabinet(program, true);
+
+	cabinet.runWith(({ x, y, value }) => {});
+
+	return cabinet.score;
+}
+
+export default new Solution(13, solution1, solution2);
